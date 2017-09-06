d27fe841667265c4d433508cc713346f96b2ef84
Peng Yu <peng.yu@shopify.com>
2017-09-06 14:41:38 -0400

    fix

--- bloom_embedding.py
+++ bloom_embedding.py
@@ -1,4 +1,3 @@
-from functools import partial
 from itertools import groupby
 import scipy.sparse as sp
 import numpy as np
@@ -13,24 +12,27 @@ SEED = None
 M = 10000
 # M targe domain capacity
 
+
 def build_mapping(old_dimension, new_dimension, seeds):
-    hash_functions = [lambda r: murmurhash3_32(r, seed=seed)%new_dimension for seed in seeds]
+    hash_functions = [lambda r: murmurhash3_32(r, seed=seed) % new_dimension for seed in seeds]
     mapping = np.zeros((old_dimension, len(seeds)))
     for d in range(old_dimension):
         for n, function in enumerate(hash_functions):
             mapping[d][n] = function(d)
     return mapping
 
+
 def bloom_embedding(X, k=K, m=M, seeds=SEED):
     seeds = seeds or range(k)
     assert len(seeds) == k
     mapping = build_mapping(X.shape[1], m, seeds)
-    new_rows, new_cols = [], []
     indptr = [0]
     indices = []
     for row, cols in groupby(zip(*X.nonzero()), key=lambda r: r[0]):
+        ones = set()
         for row, col in cols:
-            indices.extend(mapping[col])
+            ones.update(mapping[col])
+        indices.extend(ones)
         indptr.append(len(indices))
     data = np.ones(len(indices))
     return mapping, sp.csr_matrix((data, indices, indptr), dtype=int)
@@ -47,6 +49,4 @@ def reverse_embedding(mapping, Q):
         for i in range(reverse_dim):
             for j in range(k):
                 pred[row, i] += - log_Q[row, mapping[i, j]]
-    return pred 
-
-
+    return pred
